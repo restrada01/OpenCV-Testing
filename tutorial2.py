f48e244af9fc205039fbfb44b3f39fa1cd5589ed
Rafael <restrada@uwo.ca>
2022-06-03 01:12:42 -0400

    learning copying and pasting

--- tutorial2.py
+++ tutorial2.py
@@ -14,6 +14,17 @@ print(img.shape)
 
 # print(img[row]) to print a row, add [col1:col2] to get certain pixels
 
-for i in range(100):
-    for j in range(img.shape[i]):
-        img[i][j] = []
+# To randomly change top 100 pixels colors 
+
+#   for i in range(100):
+#        for j in range(img.shape[1]):
+#            img[i][j] = [random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)]
+
+# To slice out a part of an image and copy it over
+
+tag = img[700:1200, 800:1400]
+img[0:500, 0:600] = tag
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
